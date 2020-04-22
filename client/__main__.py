0e8dbd305e590341d38ad51113573ae1a0d99359
Shubhojyoti <shubhojyoti@availfinance.in>
2020-04-22 10:55:02 +0530

    bug fix

--- client/__main__.py
+++ client/__main__.py
@@ -1,14 +1,14 @@
 import logging
 import os
 import sys
-from .daemon import RunDaemon
+from .daemon import KeystrokeListener
 
 if __name__ == '__main__':
     action = sys.argv[1]
     logfile = os.path.join(os.getcwd(), "audio_chat.log")
     pidfile = os.path.join(os.getcwd(), "audio_chat.pid")
     logging.basicConfig(filename=logfile, level=logging.DEBUG)
-    d = RunDaemon(pidfile=pidfile)
+    d = KeystrokeListener(pidfile=pidfile)
 
     if action == "start":
         d.start()
